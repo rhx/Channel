2db8fcd5a50df063570ea2dba458a9b3f89da526
Rene Hexel <r.hexel@griffith.edu.au>
2017-11-26 11:54:16 +1000

    Add CircularQueue dependency

--- Package.swift
+++ Package.swift
@@ -6,23 +6,13 @@ import PackageDescription
 let package = Package(
     name: "Channel",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "Channel",
-            targets: ["Channel"]),
+        .library(name: "Channel", targets: ["Channel"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/rhx/CircularQueue.git", .branch("master")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "Channel",
-            dependencies: []),
-        .testTarget(
-            name: "ChannelTests",
-            dependencies: ["Channel"]),
+        .target(name: "Channel", dependencies: ["CircularQueue"]),
+        .testTarget(name: "ChannelTests", dependencies: ["Channel"]),
     ]
 )
